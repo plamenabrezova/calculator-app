82fd244e3b990d0459655ec88630b12a6c34347e
Plamena Mihaylova <plamena.pavlova.mihaylova@gmail.com>
2023-11-02 12:21:47 +0200

    Implement backend side of the calculator application.

---
+++ backend/src/errorHandler.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express'
+
+export function handleUndefinedRoutes(req: Request, res: Response, next: NextFunction) {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+}
+
+export function handleErrors(err: Error, req: Request, res: Response, next: NextFunction) {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+    });
+}
---
+++ backend/src/index.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import {handleErrors, handleUndefinedRoutes} from './errorHandler'
+
+const port = 3000
+
+const app = express()
+
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:4200']
+}));
+
+app.use(bodyParser.json())
+
+app.get('/sum/:num1/:num2', (req: Request, res: Response) => {
+    const {num1, num2} = req.params
+    try {
+        const result = Number(num1) + Number(num2)
+        return res.status(200).send({result})
+    } catch (err) {
+        return res.status(400).send({error: `Failed to calculate, try again.`})
+    }
+})
+
+app.get('/sub/:num1/:num2', (req: Request, res: Response) => {
+    const {num1, num2} = req.params
+    try {
+        const result = Number(num1) - Number(num2)
+        return res.status(200).send({result})
+    } catch (err) {
+        return res.status(400).send({error: `Failed to calculate, try again.`})
+    }
+})
+
+app.get('/multi/:num1/:num2', (req: Request, res: Response) => {
+    const {num1, num2} = req.params
+    try {
+        const result = Number(num1) * Number(num2)
+        return res.status(200).send({result})
+    } catch (err) {
+        return res.status(400).send({error: `Failed to calculate, try again.`})
+    }
+})
+
+app.get('/div/:num1/:num2', (req: Request, res: Response) => {
+    const {num1, num2} = req.params
+    try {
+        const result = Number(num1) / Number(num2)
+        return res.status(200).send({result})
+    } catch (err) {
+        return res.status(400).send({error: `Failed to calculate, try again.`})
+    }
+})
+
+app.use(handleUndefinedRoutes)
+app.use(handleErrors)
+
+app.listen(port, () => {
+    console.log(`Calculator server on http://localhost:${port}`)
+})
